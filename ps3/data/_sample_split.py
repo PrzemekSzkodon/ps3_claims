d071dd78b917559d7d519b3c60eaee00c42f704e
PrzemekSzkodon <przemek.szkodon@gmail.com>
2025-11-20 11:52:22 +0000

    Question 1 complete without being able to hanfdle numerous column entries

--- ps3/data/_sample_split.py
+++ ps3/data/_sample_split.py
@@ -1,12 +1,11 @@
 import hashlib
-
 import numpy as np
+import random
 
 # TODO: Write a function which creates a sample split based in some id_column and training_frac.
 # Optional: If the dtype of id_column is a string, we can use hashlib to get an integer representation.
 def create_sample_split(df, id_column, training_frac=0.8):
     """Create sample split based on ID column.
-
     Parameters
     ----------
     df : pd.DataFrame
@@ -21,5 +20,40 @@ def create_sample_split(df, id_column, training_frac=0.8):
     pd.DataFrame
         Training data with sample column containing train/test split based on IDs.
     """
+    
+    #adding a section to be able to handle multiple columns
+    
+
+    def assign_split(id_value):
+        
+        id_value = '_'.join(id_value)
+        
+        #take some id value and ensure it's a string
+        id_string = str(id_value)
+
+        #then convert to byte which is what hashlibs wants
+        id_bytes = id_string.encode()
+
+        # create a hash using the the bytes - the hash is some long hex string
+        hash_object = hashlib.md5(id_bytes)
+
+        # then we convert the hash (which is a hex string made up of letters and numbers) into a long interger
+        hash_int = int(hash_object, 16)
+
+        #then we assign the hash int into a bucket - % 100 taks the last 2 numbers, and this is a good
+        #enough approach because the hash distribution is so large and random the distirbution should be
+        # approximately uniform - note for small sample doesnt guarantee a perfect split.
+        bucket = hash_int % 100
+
+        # then we assign use the bucket to assign what value we want
+        threshold = int(training_frac * 100)
+        if bucket < threshold:
+            return 'train'
+        else:
+            return 'test'
+        
+    # now we need to call on the function to be applied to the data frame
+    # we do this by applying it to each row, using the apply function
+    df['sample'] = df['id_column'].apply(assign_split)
 
     return df
